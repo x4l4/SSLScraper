e735444f0d6d3c9b194eff3ab340666d7d76c01f
Sal <sal.napolitano89@gmail.com>
2018-04-25 19:34:52 +0200

    New
    Signed-off-by: Sal <sal.napolitano89@gmail.com>

---
+++ SSLScraper/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SSLScraper
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser3 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser3
+            // 
+            this.webBrowser3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser3.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser3.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser3.Name = "webBrowser3";
+            this.webBrowser3.Size = new System.Drawing.Size(606, 437);
+            this.webBrowser3.TabIndex = 0;
+            this.webBrowser3.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser3_DocumentCompleted);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 437);
+            this.Controls.Add(this.webBrowser3);
+            this.Name = "Form3";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.Visible = false;
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser3;
+    }
+}
+
---
+++ SSLScraper/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SSLScraper
+{
+    public partial class Form3 : Form
+    {
+        String result;
+        public Form3()
+        {
+            InitializeComponent();
+            webBrowser3.Navigate("https://www.sslproxies.org/");
+        }
+
+        private void webBrowser3_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlElementCollection collection = webBrowser3.Document.GetElementsByTagName("td");
+            foreach(HtmlElement element in collection)
+            {
+                //Console.WriteLine(element.InnerText);
+                try
+                {
+                    if (!(element.InnerText.Contains("ago")))
+                    {
+                        Match ipMatch = Regex.Match(element.InnerText, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+                        Match portMatch = Regex.Match(element.InnerText, @"\d{1,8}\b");
+                        if (ipMatch.Success)
+                            result = result + ipMatch.Value + ":";
+                        if (portMatch.Success && portMatch.Value.Contains(ipMatch.Value))
+                            result = result + portMatch.Value + "\n";
+                    }
+                }catch { }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
